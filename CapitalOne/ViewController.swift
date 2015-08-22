2941c27b1e4107e923285a5552b8abd52303b5af
chasemc67 <chasemc67@gmail.com>
2015-08-21 20:19:00 -0600

    Initial Commit

---
+++ CapitalOne/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CapitalOne
+//
+//  Created by Chase McCarty on 2015-08-21.
+//  Copyright (c) 2015 Chase McCarty. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
